7a379c74b668d663a4977f2abdb4dd7d5fa93bf6
zhangpeng53 <zhangpeng53@baidu.com>
2016-11-22 15:23:39 +0800

    add list

--- src/list.js
+++ src/list.js
@@ -2,5 +2,8 @@ const config = require('./storeConfig')
 const chalk = require('chalk')
 
 module.exports = () => {
-    
+    for(var key in config) {
+        const item = config[key];
+        console.log(chalk.bgGreen("%s : %s-%s"), key, item.desc, item.uri);
+    }
 }
